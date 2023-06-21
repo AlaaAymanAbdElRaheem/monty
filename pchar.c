0c86190710569b57471eff991a0360a195f68833
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-06-21 14:51:49 +0300

    fixing pchar

--- pchar.c
+++ pchar.c
@@ -11,7 +11,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 	(void) line_number;
 
 	if (stack == NULL || *stack == NULL)
-		print_error(3, "L", &global_var.line_number, ": can't pint, stack empty");
+		print_error(3, "L", &global_var.line_number, ": can't pchar, stack empty");
 	else if ((*stack)->n < 0 || (*stack)->n > 127)
 		print_error(3, "L", &global_var.line_number,
 			    ": can't pchar, value out of range");
